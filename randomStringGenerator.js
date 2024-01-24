1f8f1e9e6a227b8738eb9363636aa94ecc99be4d
Shanmugalakshmi11 <shanmugalakshmi.chandrasekaran@docc.techstarter.de>
2024-01-24 14:10:50 +0100

    commit

---
+++ randomStringGenerator.js
@@ -0,0 +1,6 @@
+// randomStringGenerator.js
+const randomString = require('random-string-generator');
+
+// Generate and print a random string
+const generatedString = randomString.generate();
+console.log('Generated Random String:', generatedString);
